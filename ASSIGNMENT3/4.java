50a1d69fafab91c6e3ff1b7e8b2fe99c398556d4
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 18:45:08 +0530

    Create 4.java

---
+++ ASSIGNMENT3/4.java
@@ -0,0 +1,24 @@
+//Write a Java program to check whether a number is palindrome or not.
+import java.util.Scanner;
+
+public class program4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;  
+            reversedNumber = reversedNumber * 10 + digit;  
+            number /= 10;  
+        }
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome number.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome number.");
+        }
+        scanner.close();
+    }
+}
+
